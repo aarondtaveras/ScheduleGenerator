eb8cbf20622db56af66d6aaf4d4953f87fe2bd81
Aaron <aarondtaveras@gmail.com>
2017-11-09 22:41:40 -0500

    Added generateDictionary function, as well as test function and file. Currently works on test values.

--- src/schedule-generator.ts
+++ src/schedule-generator.ts
@@ -1,13 +1,34 @@
 import {ScheduleEvent} from './datamodels/schedule-event';
+import {testGenerateDictionary} from './tests/test-generate-dictionary'
 
 let testEvents:ScheduleEvent[] = [];
 let testEvents2:ScheduleEvent[] = [];
 let e = new ScheduleEvent(20, 30, 'section 310', 'Math course');
-let e2 = new ScheduleEvent(20, 30, 'section 310', 'Math course');
+let e3 = new ScheduleEvent(40,50,'section 250','Math course');
+let e4 = new ScheduleEvent(10,20,'section 290','English course')
+let e2 = new ScheduleEvent(30, 30, 'section 310', 'Math course');
 testEvents.push(e);
 testEvents2.push(e);
-
+testEvents.push(e2);
+testEvents.push(e3);
+testEvents.push(e4);
 testEvents2[0].eventName = "happy";
 
-console.log(testEvents)
-console.log(testEvents2)
+var dictionary = {};
+
+console.log(testEvents);
+console.log(testEvents2);
+
+export function generateDictionary(schedule:ScheduleEvent[]){
+for(let i:number=0;i<schedule.length;i++){
+    if(!dictionary[schedule[i].eventGroup]){
+        dictionary[schedule[i].eventGroup] = [schedule[i]];
+    } else {
+        dictionary[schedule[i].eventGroup].push(schedule[i]);
+    }
+    
+}
+    console.log(dictionary);
+}
+
+testGenerateDictionary();
---
+++ src/tests/test-generate-dictionary.ts
@@ -0,0 +1,28 @@
+import {ScheduleEvent} from '../datamodels/schedule-event';
+import {generateDictionary} from '../schedule-generator';
+
+
+
+
+
+export function testGenerateDictionary(){
+    let testSchedule:ScheduleEvent[] = [];
+    let g1:string = 'Math course';
+    let g2:string = 'English course';
+    let g3:string = 'Science course';
+   for(let i:number = 0;i<20;i++){
+    if(i>=0 && i<5){
+    let e = new ScheduleEvent(i,i,'section' + i,g1);
+    testSchedule.push(e);
+    }
+    else if(i>=5 && i<=12){
+    let e = new ScheduleEvent(i,i,'section' + i,g2);
+    testSchedule.push(e);
+    }
+    else{
+    let e = new ScheduleEvent(i,i,'section' + i,g3);
+    testSchedule.push(e);
+    }
+   }
+   generateDictionary(testSchedule);
+}
